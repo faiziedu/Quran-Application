214cbb46aa13346fb981da73521f3a8935629fe9
Muhammad Faizan Akram <faiziedu977@gmail.com>
2023-06-18 18:27:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/qurantranslation/Search.java
@@ -0,0 +1,34 @@
+package com.example.qurantranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search extends AppCompatActivity {
+
+    int start;
+
+    int range;
+
+    EditText first ;
+
+    Button second;
+
+    TextView third;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+
+    }}
